5d50a01b8e257e49f3aba304d853191ddbddc4b1
Eleanor Davies <ellie@mavieson.co.uk>
2018-11-21 15:42:49 +0000

    first

---
+++ README.md
@@ -0,0 +1,4 @@
+# Ignitenet Metrolinq link data scraper
+
+Need to put geckodriver in /usr/local/bin so it can be executed.
+cp geckodriver /usr/local/bin/geckodriver
---
+++ scrape.py
@@ -0,0 +1,88 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+import json
+
+# Start FireFox in headless mode
+options = Options()
+options.headless = True
+driver = webdriver.Firefox(options=options)
+
+# Connect to login page
+driver.get("http://172.17.55.8/cgi-bin/acn/")
+print ("Headless Firefox Initialized")
+
+# load credentials from file
+with open('credentials.json') as cred:
+    credentials = json.load(cred)
+
+# Pass in login credentials
+username = driver.find_element_by_name('username')
+username.send_keys(credentials["username"])
+password = driver.find_element_by_name('password')
+password.send_keys(credentials["password"])
+signInButton = driver.find_element_by_id('Apply')
+signInButton.click()
+# driver.get_screenshot_as_file('ignite.png')
+
+# This var holds the base URL with the token needed for authentication
+token_url = driver.current_url
+
+# take the address of the script and a name to save as.
+def get_json(addr, name):
+    driver.get(token_url + addr)
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+    element = soup.get_text("div", {"id": "json"})
+    print(element)
+    f = open('%s.json' % name, 'w')
+    f.write(element)
+    f.close()
+    return json.loads(element)
+
+# get json for both 5ghz and 60ghz
+sixty_json = get_json("/admin/status/connected_clients_metro?ifname=radio1&freq=60", "60ghz")
+five_json = get_json("/admin/status/connected_clients?ifname=ath0", "5ghz")
+
+# Need a separate json file for each mac
+# Init the arrays
+try:
+    with open('5ghz_MAC.json') as five_ghz_mac_raw:
+        five_ghz_mac = json.load(five_ghz_mac_raw)
+except:
+    five_ghz_mac = {}
+
+try:
+    with open('60ghz_MAC.json', newline='') as sixty_ghz_mac_raw:
+        sixty_ghz_mac = json.load(sixty_ghz_mac_raw)
+except:
+    sixty_ghz_mac = {}
+
+
+if (sixty_ghz_mac == {}):
+    print(five_ghz_mac)
+
+print("but we continue")
+
+
+
+# driver.get(token_url + "/admin/status/connected_clients_metro?ifname=radio1&freq=60")
+# # driver.get_screenshot_as_file('ignite.png')
+# soup = BeautifulSoup(driver.page_source, "html.parser")
+# element = soup.get_text("div", {"id": "json"})
+# print(element)
+# f = open('60ghz.json', 'w')
+# f.write(element)
+# f.close()
+#
+# driver.get(token_url + "/admin/status/connected_clients?ifname=ath0")
+# soup = BeautifulSoup(driver.page_source, "html.parser")
+# element = soup.get_text("div", {"id": "json"})
+# print(element)
+# f = open('5ghz.json', 'w')
+# f.write(element)
+# f.close()
+
+driver.quit()
